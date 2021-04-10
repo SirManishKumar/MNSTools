8a5dd32f96ab696713112dbcabb90344181f742a
Manish Kumar <79128476+SirManishKumar@users.noreply.github.com>
2021-04-10 20:59:45 +0530

    Create install.py

---
+++ install.py
@@ -0,0 +1,151 @@
+
+import os,sys,time
+#sys1
+def jalan(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(00000.02)
+os.system('clear')
+
+#this logo
+logo = ("""\033[1;32m──────────────────────────────────────────────────────────────────────────────────
+\033[1;32m                           __
+          ____  _            |  |        __  __ _   _  ____
+         / ___|(_)_ __    ___|  |___    |  \/  | \ | |/ ___|
+         \___ \| | `__|   \__    __/    | |\/| |  \| |\___ \
+          ___) | | |         \  /       | |  | | |\  | ___) |
+  __  __ |____/|_|_| _     _  \/ _  __  |_|  |_|_| \_||____/
+ |  \/  | __ _ _ __ (_)___| |__ | |/ /   _ _ __ ___   __ _ _ _
+ | |\/| |/ _` | `_ \| / __| `_ \| ` / | | | `_ ` _ \ / _` | `_|
+ | |  | | (_| | | | | \__ \ | | | . \ |_| | | | | | | (_| | |
+ |_|  |_|\__,_|_| |_|_|___/_| |_|_|\_\__,_|_| |_| |_|\__,_|_|
+╭━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━╮
+   Created By:- https://github.com/SirManishKumar/TermuxLock
+     Follow On Twitter:- http://twtter.com/SirManishKumar
+╰━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━╯""")
+#my logo
+print(logo)
+jalan("###############################################################################")
+print("")
+print("\033[1;32mPlease don't forget to subscribe to my YouTube channel")
+time.sleep(6)
+
+os.system('xdg-open https://www.youtube.com/mnspandit')
+print("\033[1;31m{\033[1;33mmy youtube channel: SirManishKumar\033[1;31m}")
+print("{\033[1;33mmy github account : SirManishKumar\033[1;31m}")
+print("{\033[1;33mmy facebook SirManishKumar\033[1;31m}")
+def jalan(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(00000.08)
+jalan ("                       \033[1;31mThank you for using my tool            ")
+def jalan(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(00000.02)
+
+#welcom
+jalan ("\033[1;31m -_-_-__-_-_-_-_-_-_-_-_-_-_-_&&&\033[1;38mHello in my World\033[1;31m&&&_-_-_-_-_-_-_-_-_-_-_-_-_-_-_")
+jalan("_____________((\033[1;38mThis̲ tool by SirManishKumar\033[1;31m))___________")
+jalan("__________________((\033[1;38mIt's All Tools My Favorite\033[1;31m))________________")
+jalan("____________((\033[1;38mI'm From \033[1;32mIndia))________")
+print ("\033[1;31m {0}  \033[1;32m exit")
+print ("\033[1;31m {1}  \033[1;32m Download All Pkg ")
+print ("\033[1;31m {2}  \033[1;32m Download  ")
+print ("\033[1;31m {3}  \033[1;32m Download Ngrok ")
+print ("\033[1;31m {4}  \033[1;32m Download Termux-ADB ")
+print ("\033[1;31m {5}  \033[1;32m Download PhoneSploit ")
+print ("\033[1;31m {6}  \033[1;32m Downloadّ Saycheese ")
+nmb = input("\033[1;32mChoose number : ")
+if nmb=="0":
+    os.system('exit')
+    os.system('clear')
+
+#Download All Pkg
+elif nmb=="1":
+    os.system('apt update && apt upgrade -y ')
+    os.system('pkg update && upgrade ')
+    os.system('pkg install git -y' )
+    os.system('pkg install python -y ')
+    os.system('pkg install python2 -y ')
+    os.system('pkg install unzip -y ')
+    os.system('pkg install nano -y ')
+    os.system('pkg install figlet -y ')
+    os.system('pkg install curl -y ')
+    os.system('pkg install wget -y ')
+    os.system('pkg install php -y ')
+    os.system('pkg install ruby -y ')
+    os.system('pip install --upgrade pip ')
+    os.system('pkg install perl -y ')
+    os.system('pkg install zip -y ')
+    os.system('pkg install unzip -y ')
+    os.system('pkg install openssl -y ')
+    os.system('pkg install cmatrix -y ')
+    os.system('pkg install openssh ')
+    os.system('pip install colorama ')
+    os.system('pip2 install requests ')
+    os.system('clear ')
+    os.system('apt update && apt upgrade -y ')
+    os.system('clear ')
+    os.system('figlet sniper-gadgets ')
+    time.sleep(000.5)
+    os.system('clear ')
+    jalan ("\033[1;31m             Download All Pkg It's finished Thank you for using my tool       ")
+
+#Download Ngrok
+elif nmb=="2":
+    os.system('apt update && apt upgrade')
+    os.system('pkg install wget ')
+    os.system('wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip ')
+    os.system('unzip ngrok-stable-linux-arm.zip ')
+    os.system('chmod 777 ngrok ')
+    os.system('rm ngrok-stable-linux-arm.zip ')
+    jalan("\033[1;31m                Download Ngrok It's finished Thank you for using my tool         ")
+
+#Download Ngrok
+elif nmb=="3":
+    os.system('apt update && apt upgrade')
+    os.system('pkg install wget ')
+    os.system('wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip ')
+    os.system('unzip ngrok-stable-linux-arm.zip ')
+    os.system('chmod 777 ngrok ')
+    os.system('rm ngrok-stable-linux-arm.zip ')
+    jalan("\033[1;31m                Download Ngrok It's finished Thank you for using my tool         ")
+
+#Download Termux-ADB
+elif nmb=="4":
+    os.system('apt update && apt upgrade')
+    os.system('pkg install wget ')
+    os.system('git clone https://github.com/MasterDevX/Termux-ADB ')
+    os.system('cd Termux-ADB ')
+    os.system('chmod +x InstallTools.sh ')
+    os.system('bash InstallTools.sh ')
+    jalan ("\033[1;31m                Download Termux-ADB It's finished Thank you for using my tool           ")
+
+#Download PhoneSploit
+elif nmb=="5":
+    os.system('apt update && apt upgrade')
+    os.system('pkg install git ')
+    os.system('git clone https://github.com/01010000-kumar/PhoneSploit ')
+    os.system('cd PhoneSploit ')
+    os.system('chmod +x phonesploit.py ')
+    os.system('python phonesploit.py ')
+    jalan ("\033[1;31m                Download PhoneSploit It's finished Thank you for using my tool           ")
+
+#Download Saycheese
+elif nmb=="6":
+    os.system('apt update && apt upgrade')
+    os.system('git clone https://github.com/Technicalheadquarter/saycheese ')
+    os.system('cd saycheese ')
+    os.system('chmod +x saycheese.sh ')
+    os.system('./saycheese.sh ')
+    jalan ("\033[1;31m                  Download Saycheese It's finished Thank you for using my tool               ")
+
+#Try ouvert my tool
+else :
+    print ("\033[1;31m                  X_False number try again_X         ")
+    time.sleep(00000.10)
+    os.system('python sniper-gadgets.py ')
