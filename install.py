7042d76369eaa84a459b3b29bb1ad43f1279c413
Manish Kumar <79128476+SirManishKumar@users.noreply.github.com>
2021-04-10 22:46:19 +0530

    Update install.py

--- install.py
+++ install.py
@@ -26,7 +26,7 @@ logo = ("""\033[1;32m───────────────────�
 ╰━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━━ ━━━━━╯""")
 #my logo
 print(logo)
-jalan("###############################################################################")
+jalan("################################################################")
 print("")
 print("\033[1;32mPlease don't forget to subscribe to my YouTube channel")
 time.sleep(6)
@@ -40,7 +40,7 @@ def jalan(z):
         sys.stdout.write(e)
         sys.stdout.flush()
         time.sleep(00000.08)
-jalan ("                       \033[1;31mThank you for using my tool            ")
+jalan ("               \033[1;31mThank you for using my tool            ")
 def jalan(z):
     for e in z + '\n':
         sys.stdout.write(e)
@@ -48,17 +48,26 @@ def jalan(z):
         time.sleep(00000.02)
 
 #welcom
-jalan ("\033[1;31m -_-_-__-_-_-_-_-_-_-_-_-_-_-_&&&\033[1;38mHello in my World\033[1;31m&&&_-_-_-_-_-_-_-_-_-_-_-_-_-_-_")
-jalan("_____________((\033[1;38mThis̲ tool by SirManishKumar\033[1;31m))___________")
-jalan("__________________((\033[1;38mIt's All Tools My Favorite\033[1;31m))________________")
-jalan("____________((\033[1;38mI'm From \033[1;32mIndia))________")
-print ("\033[1;31m {0}  \033[1;32m exit")
-print ("\033[1;31m {1}  \033[1;32m Download All Pkg ")
-print ("\033[1;31m {2}  \033[1;32m Download  ")
-print ("\033[1;31m {3}  \033[1;32m Download Ngrok ")
-print ("\033[1;31m {4}  \033[1;32m Download Termux-ADB ")
+jalan ("\033[1;31m-_-_-_-_-_-_-_-_-_-_ \033[1;38mHello in my World\033[1;31m _-_-_-_-_-_-_-_-_-_-")
+jalan("_______________((\033[1;38mThis̲ tool by SirManishKumar\033[1;31m))______________")
+jalan("_______________((\033[1;38mIt's All Tools My Favorite\033[1;31m))_______________")
+jalan("___________________((\033[1;38mI'm From \033[1;32mIndia))__________________")
+print ("\033[1;31m {0}  \033[1;32m exit ")                    ("\033[1;31m {16}  \033[1;32m Download Cam-Hackers ")
+print ("\033[1;31m {1}  \033[1;32m Download All Pkg ")        ("\033[1;31m {17}  \033[1;32m Download Anon-SMS ")
+print ("\033[1;31m {2}  \033[1;32m Download AdvPhishing ")    ("\033[1;31m {18}  \033[1;32m Download Tool-X ")
+print ("\033[1;31m {3}  \033[1;32m Download Ngrok ")          ("\033[1;31m {19}  \033[1;32m Download Metasploit-Installation ")
+print ("\033[1;31m {4}  \033[1;32m Download Termux-ADB ")     ("\033[1;31m {20}  \033[1;32m Download Lazymux ")
 print ("\033[1;31m {5}  \033[1;32m Download PhoneSploit ")
 print ("\033[1;31m {6}  \033[1;32m Downloadّ Saycheese ")
+print ("\033[1;31m {7}  \033[1;32m Download Seeker ")
+print ("\033[1;31m {8}  \033[1;32m Download Grabcam ")
+print ("\033[1;31m {9}  \033[1;32m Download Hacklock ")
+print ("\033[1;31m {10} \033[1;32m Download Zphisher ")
+print ("\033[1;31m {11} \033[1;32m Download FotoSploit ")
+print ("\033[1;31m {12} \033[1;32m Downloadّ EasY_HaCk ")
+print ("\033[1;31m {13} \033[1;32m Download T-Phish ")
+print ("\033[1;31m {14} \033[1;32m Download Sayhello ")
+print ("\033[1;31m {15} \033[1;32m Downloadّ Lockphish ")
 nmb = input("\033[1;32mChoose number : ")
 if nmb=="0":
     os.system('exit')
@@ -87,23 +96,25 @@ elif nmb=="1":
     os.system('pkg install openssh ')
     os.system('pip install colorama ')
     os.system('pip2 install requests ')
+    os.system('pip3 install requests ')
     os.system('clear ')
     os.system('apt update && apt upgrade -y ')
     os.system('clear ')
     os.system('figlet sniper-gadgets ')
     time.sleep(000.5)
     os.system('clear ')
-    jalan ("\033[1;31m             Download All Pkg It's finished Thank you for using my tool       ")
+    jalan ("\033[1;31m        Download All Pkg It's finished Thank you for using my tool       ")
 
-#Download Ngrok
+#Download AdvPhishing
 elif nmb=="2":
     os.system('apt update && apt upgrade')
-    os.system('pkg install wget ')
-    os.system('wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip ')
-    os.system('unzip ngrok-stable-linux-arm.zip ')
-    os.system('chmod 777 ngrok ')
-    os.system('rm ngrok-stable-linux-arm.zip ')
-    jalan("\033[1;31m                Download Ngrok It's finished Thank you for using my tool         ")
+    os.system('pkg install git ')
+    os.system('git clone https://github.com/Ignitetch/AdvPhishing ')
+    os.system('cd AdvPhishing ')
+    os.system('chmod +x * ')
+    os.system('bash Android-Setup.sh ')
+    os.system('bash AdvPhishing.sh ')
+    jalan("\033[1;31m        Download AdvPhishing It's finished Thank you for using my tool         ")
 
 #Download Ngrok
 elif nmb=="3":
@@ -113,7 +124,7 @@ elif nmb=="3":
     os.system('unzip ngrok-stable-linux-arm.zip ')
     os.system('chmod 777 ngrok ')
     os.system('rm ngrok-stable-linux-arm.zip ')
-    jalan("\033[1;31m                Download Ngrok It's finished Thank you for using my tool         ")
+    jalan("\033[1;31m        Download Ngrok It's finished Thank you for using my tool         ")
 
 #Download Termux-ADB
 elif nmb=="4":
@@ -123,7 +134,7 @@ elif nmb=="4":
     os.system('cd Termux-ADB ')
     os.system('chmod +x InstallTools.sh ')
     os.system('bash InstallTools.sh ')
-    jalan ("\033[1;31m                Download Termux-ADB It's finished Thank you for using my tool           ")
+    jalan ("\033[1;31m        Download Termux-ADB It's finished Thank you for using my tool           ")
 
 #Download PhoneSploit
 elif nmb=="5":
@@ -133,7 +144,7 @@ elif nmb=="5":
     os.system('cd PhoneSploit ')
     os.system('chmod +x phonesploit.py ')
     os.system('python phonesploit.py ')
-    jalan ("\033[1;31m                Download PhoneSploit It's finished Thank you for using my tool           ")
+    jalan ("\033[1;31m        Download PhoneSploit It's finished Thank you for using my tool           ")
 
 #Download Saycheese
 elif nmb=="6":
@@ -142,10 +153,144 @@ elif nmb=="6":
     os.system('cd saycheese ')
     os.system('chmod +x saycheese.sh ')
     os.system('./saycheese.sh ')
-    jalan ("\033[1;31m                  Download Saycheese It's finished Thank you for using my tool               ")
+    jalan ("\033[1;31m        Download Saycheese It's finished Thank you for using my tool               ")
+
+#Download Seeker
+elif nmb=="7":
+    os.system('apt update && apt upgrade')
+    os.system('git clone https://github.com/thewhiteh4t/seeker ')
+    os.system('cd seeker ')
+    os.system('pip install requests ')
+    os.system('chmod +x seeker.py ')
+    os.system('python seeker.py -t manual ')
+    jalan ("\033[1;31m        Download Seeker It's finished Thank you for using my tool               ")
+
+#Download Grabcam
+elif nmb=="8":
+    os.system('apt update && apt upgrade')
+    os.system('pip install lolcat ')
+    os.system('git clone https://github.com/noob-hackers/grabcam ')
+    os.system('cd grabcam ')
+    os.system('chmod +x grabcam.sh ')
+    os.system('bash grabcam.sh ')
+    jalan ("\033[1;31m        Download Grabcam It's finished Thank you for using my tool               ")
+
+#Download Hacklock
+elif nmb=="9":
+    os.system('apt update && apt upgrade')
+    os.system('pip install lolcat ')
+    os.system('git clone https://github.com/noob-hackers/hacklock ')
+    os.system('cd hacklock ')
+    os.system('chmod +x * ')
+    os.system('bash setup ')
+    jalan ("\033[1;31m        Download Hacklock It's finished Thank you for using my tool               ")
+
+#Download Zphisher
+elif nmb=="10":
+    os.system('apt update && apt upgrade')
+    os.system('git clone git://github.com/htr-tech/zphisher.git ')
+    os.system('cd zphisher ')
+    os.system('chmod +x saycheese.sh ')
+    os.system('bash zphisher.sh ')
+    jalan ("\033[1;31m        Download Zphisher It's finished Thank you for using my tool               ")
+
+#Download FotoSploit
+elif nmb=="11":
+    os.system('apt update && apt upgrade')
+    os.system('git clone https://github.com/Cesar-Hack-Gray/FotoSploit ')
+    os.system('cd FotoSploit ')
+    os.system('chmod +x * ')
+    os.system('bash install.sh ')
+    os.system('./FotoSploit ')
+    jalan ("\033[1;31m        Download FotoSploit It's finished Thank you for using my tool               ")
+
+#Download EasY_HaCk
+elif nmb=="12":
+    os.system('apt update && apt upgrade')
+    os.system('git clone https://github.com/sabri-zaki/EasY_HaCk ')
+    os.system('cd EasY_HaCk ')
+    os.system('chmod +x install.sh ')
+    os.system('./install.sh ')
+    jalan ("\033[1;31m        Download EasY_HaCk It's finished Thank you for using my tool               ")
+
+#Download T-Phish
+elif nmb=="13":
+    os.system('apt update && apt upgrade')
+    os.system('git clone https://github.com/Stephin-Franklin/T-Phish ')
+    os.system('cd T-Phish ')
+    os.system('unzip T-Phish.zip ')
+    os.system('cd T-Phish ')
+    os.system('chmod +x * ')
+    os.system('./start.sh ')
+    os.system('./phish.sh ')
+    jalan ("\033[1;31m        Download T-Phish It's finished Thank you for using my tool               ")
+
+#Download Sayhello
+elif nmb=="14":
+    os.system('apt update && apt upgrade')
+    os.system('git clone https://github.com/deepakverma91123/sayhellodeepak.git ')
+    os.system('cd sayhellodeepak ')
+    os.system('chmod +x sayhello.sh ')
+    os.system('bash sayhello.sh ')
+    jalan ("\033[1;31m        Download Sayhello It's finished Thank you for using my tool               ")
+
+#Download Lockphish
+elif nmb=="15":
+    os.system('apt update && apt upgrade')
+    os.system('git clone https://github.com/kali-linux-tutorial/lockphish ')
+    os.system('cd lockphish ')
+    os.system('chmod +x lockphish.sh ')
+    os.system('bash lockphish.sh ')
+    jalan ("\033[1;31m        Download Lockphish It's finished Thank you for using my tool               ")
+
+#Download Cam-Hackers
+elif nmb=="16":
+    os.system('apt update && apt upgrade')
+    os.system('git clone https://github.com/AngelSecurityTeam/Cam-Hackers ')
+    os.system('cd Cam-Hackers ')
+    os.system('chmod +x cam-hackers.py ')
+    os.system('python3 cam-hackers.py ')
+    jalan ("\033[1;31m        Download Cam-Hackers It's finished Thank you for using my tool               ")
+
+#Download Anon-SMS
+elif nmb=="17":
+    os.system('apt update && apt upgrade')
+    os.system('git clone https://github.com/HACK3RY2J/Anon-SMS.git ')
+    os.system('cd Anon-SMS ')
+    os.system('chmod +x * ')
+    os.system('bash Run.sh ')
+    jalan ("\033[1;31m        Download Anon-SMS It's finished Thank you for using my tool               ")
+
+#Download Tool-X
+elif nmb=="18":
+    os.system('apt update && apt upgrade')
+    os.system('git clone https://github.com/Rajkumrdusad/Tool-X.git ')
+    os.system('cd Tool-X ')
+    os.system('chmod +x install.aex ')
+    os.system('./install.aex ')
+    jalan ("\033[1;31m        Download Tool-X It's finished Thank you for using my tool               ")
+
+#Download Metasploit-Installation
+elif nmb=="19":
+    os.system('apt update && apt upgrade')
+    os.system('git clone http://github.com/rooted-cyber/Metasploit-Installation ')
+    os.system('cd Metasploit-Installation ')
+    os.system('chmod +x setup.sh ')
+    os.system('bash setup.sh ')
+    os.system('bash metasploit.sh ')
+    jalan ("\033[1;31m        Download Metasploit-Installation It's finished Thank you for using my tool               ")
+
+#Download Lazymux
+elif nmb=="20":
+    os.system('apt update && apt upgrade')
+    os.system('git clone https://github.com/Gameye98/Lazymux ')
+    os.system('cd Lazymux ')
+    os.system('chmod +x lazymux.py ')
+    os.system('python lazymux.py ')
+    jalan ("\033[1;31m        Download Lazymux It's finished Thank you for using my tool               ")
 
 #Try ouvert my tool
 else :
     print ("\033[1;31m                  X_False number try again_X         ")
     time.sleep(00000.10)
-    os.system('python sniper-gadgets.py ')
+    os.system('python install.py ')
